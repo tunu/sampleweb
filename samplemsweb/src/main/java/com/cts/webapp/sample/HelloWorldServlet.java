4b28a75f3f23b08bf54c2cba8c5803c6dcf08bd9
tunu <tunu321@gmail.com>
2015-09-30 15:27:46 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -17,23 +17,12 @@ public class HelloWorldServlet extends HttpServlet {
 			throws ServletException, IOException {
 		
 		resp.setContentType("text/html");
-		
 		PrintWriter out = resp.getWriter();
 		
-		<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-814714-1', 'auto');
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
-
-</script>
+		
 
 		
-		out.println("<body style='background-color:#d3d3d3;'>");
+		out.println("<body style='background-color:#0040FF;'>");
 		out.print("<h1> Integrating code change state3 .."+new java.util.Date()+"</h1>");
 		out.flush();
 		out.close();	}
