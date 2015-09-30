7a131ba95471865c1612c38c24e1aa0cf92b432e
tunu <tunu321@gmail.com>
2015-09-30 15:14:23 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -16,27 +16,10 @@ public class HelloWorldServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		
-		out.println("<!DOCTYPE html>");  // HTML 5
-         out.println("<html><head>");
-         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
-         String title = rb.getString("Integrating code change state2.title");
-out.println("<body style='background-color:#d3d3d3;'>");
-
-		out.print("<h1>Date::"+new java.util.Date()+"</h1>");
-		out.println("<html><body bgcolor="+c1+"> <font color="+ c2 +"> Integrating code change state2  </font> </body></html>");
-         out.println("<title>" + title + "</title></head>");
-         out.println("<body>");
-         out.println("<h1>" + title + "</h1>");  
-         
-         out.println("<a href='" + request.getRequestURI() + "'><img src='images/return.gif'></a>");
-         out.println("</body></html>");
-		
-		
-		
 		resp.setContentType("text/html");
 		
 		PrintWriter out = resp.getWriter();
+		out.println("<body style='background-color:#d3d3d3;'>");
 		out.print("<h1> Integrating code change state3 .."+new java.util.Date()+"</h1>");
 		out.flush();
 		out.close();	}
