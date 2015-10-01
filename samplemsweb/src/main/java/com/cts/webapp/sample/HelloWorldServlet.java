697be9aecf588749c0116f78b82d47120d4bc03a
tunu <tunu321@gmail.com>
2015-10-01 15:12:48 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -22,10 +22,10 @@ public class HelloWorldServlet extends HttpServlet {
 		out.println("<body style='background-color:#82CEE5;'>");
 	//	out.println("<marquee width=200 direction=left><img src="http://www.krazy-kreations.com/images/babies/0059.gif"></marquee>");
 		out.print("<h1><marquee> Integrating code change on DOCKER container </marquee>"+new java.util.Date()+"</h1>");
-		out.print("<h1><marquee>Integrating code change state3</marquee>..</h1>");
-		out.print("<h1><marquee direction=right> Docker helps rapid application deployment </marquee>..</h1>");
-		out.print("<h1><marquee behavior=alternate> Docker helps portability across machines </marquee>..+new java.util.Date()+</h1>");
-	//	out.print("<h1><marquee direction="up" behavior="alternate" style="height:1000px"> Docker helps sharing and minimal overhead </marquee>.."+new java.util.Date()+"</h1>");
+	//	out.print("<h1><marquee>Integrating code change state3</marquee>..</h1>");
+		out.print("<h1><marquee direction=right> Docker helps rapid application deployment </marquee></h1>");
+		out.print("<h1><marquee behavior=alternate> Docker helps portability across machines </marquee></h1>");
+		out.print("<h1><marquee direction=up behavior=alternate style=height:1000px> Docker helps sharing and minimal overhead </marquee></h1>");
 	//	out.print("<h1><marquee style="border:RED 3px SOLID"> Docker containers are lightweight </marquee>.."+new java.util.Date()+"</h1>");
 	//	out.print("<h1><marquee> Shippable <img src="https://www.docker.com/sites/all/themes/docker/assets/images/logo.png" width="150" height="100" alt="Tutorials" border="0"></marquee>"+new java.util.Date()+"</h1>");
 		
