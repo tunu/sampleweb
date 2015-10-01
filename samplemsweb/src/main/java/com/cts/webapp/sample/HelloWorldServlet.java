9b785a04dbcbbc6a5f684271fa4847ac5ab64686
tunu <tunu321@gmail.com>
2015-10-01 14:21:46 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -20,7 +20,7 @@ public class HelloWorldServlet extends HttpServlet {
 		PrintWriter out = resp.getWriter();
 		
 		out.println("<body style='background-color:#82CEE5;'>");
-	//	out.println(""<marquee width=200 direction=left><img src="http://www.krazy-kreations.com/images/babies/0059.gif"></marquee>");
+		out.println(""<marquee width=200 direction=left><img src="http://www.krazy-kreations.com/images/babies/0059.gif"></marquee>");
 		out.print("<h1><marquee> Integrating code change state3 </marquee>.."+new java.util.Date()+"</h1>");
 	//	out.print("<h1><marquee direction="right"> Docker helps rapid application deployment </marquee>.."+new java.util.Date()+"</h1>");
 	//	out.print("<h1><marquee behavior="alternate"> Docker helps portability across machines </marquee>.."+new java.util.Date()+"</h1>");
