f37f81518c313bc2498cab140a69944841ec184d
tunu <tunu321@gmail.com>
2015-10-01 14:20:26 +0530

    Update HelloWorldServlet.java

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -20,13 +20,13 @@ public class HelloWorldServlet extends HttpServlet {
 		PrintWriter out = resp.getWriter();
 		
 		out.println("<body style='background-color:#82CEE5;'>");
-		out.println(""<marquee width=200 direction=left><img src="http://www.krazy-kreations.com/images/babies/0059.gif"></marquee>");
+	//	out.println(""<marquee width=200 direction=left><img src="http://www.krazy-kreations.com/images/babies/0059.gif"></marquee>");
 		out.print("<h1><marquee> Integrating code change state3 </marquee>.."+new java.util.Date()+"</h1>");
-		out.print("<h1><marquee direction="right"> Docker helps rapid application deployment </marquee>.."+new java.util.Date()+"</h1>");
-		out.print("<h1><marquee behavior="alternate"> Docker helps portability across machines </marquee>.."+new java.util.Date()+"</h1>");
-		out.print("<h1><marquee direction="up" behavior="alternate" style="height:1000px"> Docker helps sharing and minimal overhead </marquee>.."+new java.util.Date()+"</h1>");
-		out.print("<h1><marquee style="border:RED 3px SOLID"> Docker containers are lightweight </marquee>.."+new java.util.Date()+"</h1>");
-		out.print("<h1><marquee> Shippable <img src="https://www.docker.com/sites/all/themes/docker/assets/images/logo.png" width="150" height="100" alt="Tutorials" border="0"></marquee>"+new java.util.Date()+"</h1>");
+	//	out.print("<h1><marquee direction="right"> Docker helps rapid application deployment </marquee>.."+new java.util.Date()+"</h1>");
+	//	out.print("<h1><marquee behavior="alternate"> Docker helps portability across machines </marquee>.."+new java.util.Date()+"</h1>");
+	//	out.print("<h1><marquee direction="up" behavior="alternate" style="height:1000px"> Docker helps sharing and minimal overhead </marquee>.."+new java.util.Date()+"</h1>");
+	//	out.print("<h1><marquee style="border:RED 3px SOLID"> Docker containers are lightweight </marquee>.."+new java.util.Date()+"</h1>");
+	//	out.print("<h1><marquee> Shippable <img src="https://www.docker.com/sites/all/themes/docker/assets/images/logo.png" width="150" height="100" alt="Tutorials" border="0"></marquee>"+new java.util.Date()+"</h1>");
 		
 		out.flush();
 		out.close();	}
