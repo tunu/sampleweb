5e8afcc222cbed49bbeb7be8c57cd9ca60d3be23
tunu <tunu321@gmail.com>
2015-10-25 11:13:07 +0530

    cal2

--- samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
+++ samplemsweb/src/main/java/com/cts/webapp/sample/HelloWorldServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 //public class HelloWorldServlet extends HttpServlet {
 	
 	
-	private static final long serialVersionUID = 1031422249396784970L;
+//	private static final long serialVersionUID = 1031422249396784970L;
 
 //	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 //			throws ServletException, IOException {
